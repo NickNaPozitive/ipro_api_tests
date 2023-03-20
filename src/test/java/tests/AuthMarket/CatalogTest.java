1cd42038786438fba22ced7aac5eae69f72e25cc
NickNaPoztive <20101999dN>
2023-03-20 13:47:14 +0300

    more tests

--- src/test/java/tests/AuthMarket/CatalogTest.java
+++ src/test/java/tests/AuthMarket/CatalogTest.java
@@ -4,6 +4,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.ApiCoreRequests;
 import lib.Assertions;
@@ -13,12 +14,15 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CatalogTest extends BaseTestCase {
 
     private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+    String urlForCatalog = "md6J4_hh4kG6YXLcEw99d";
 
     @Test
     @Owner(value = "Дегтярёв Никита Витальевич")
@@ -37,8 +41,9 @@ public class CatalogTest extends BaseTestCase {
 
         //CHECK CNT
         Response responseGetCntFavorites = apiCoreRequests.
-                makeGetRequestWithNoBody("https://idev.etm.ru/_next/data/HvB8kSbg34YbZlhTt2Inw/catalog.json?my_fav=1");
+                makeGetRequestWithNoBody("https://idev.etm.ru/_next/data/" + urlForCatalog + "/catalog.json?my_fav=1");
         String cnt = getSingleHeaderFromJson(responseGetCntFavorites, "pageProps", ".data.records");
+        System.out.println(cnt);
 
 
         //ADDING
@@ -51,7 +56,7 @@ public class CatalogTest extends BaseTestCase {
         Response responseDelProduct = apiCoreRequests
                 .makeGetRequestWithNoBody("https://idev.etm.ru/api/ipro/goods/" + idGoods + "/del_from_fav?session-id=" + sessionId);
         Assertions.assertResponseCodeEquals(responseDelProduct, 200);
-        Assertions.assertNotJsonByName(responseDelProduct, "data", ".cnt", cnt);
+        Assertions.assertJsonByName(responseDelProduct, "data", ".cnt", cnt);
     }
 
     @Test
@@ -99,7 +104,7 @@ public class CatalogTest extends BaseTestCase {
 
         //TAKE DATA FROM CATALOG
         Response responseGetDataRem = apiCoreRequests.
-                makeGetRequestWithNoBody("https://idev.etm.ru/_next/data/HvB8kSbg34YbZlhTt2Inw/catalog/101010_kabeli_s_mednoy_tokoprovodjaschey_zhiloy.json?nameCat=101010_kabeli_s_mednoy_tokoprovodjaschey_zhiloy");
+                makeGetRequestWithNoBody("https://idev.etm.ru/_next/data/" + urlForCatalog + "/catalog/101010_kabeli_s_mednoy_tokoprovodjaschey_zhiloy.json?nameCat=101010_kabeli_s_mednoy_tokoprovodjaschey_zhiloy");
 
         List rems = getSingleValueFromJson(responseGetDataRem, "pageProps", ".data.rows");
         String rem = "";
@@ -117,6 +122,74 @@ public class CatalogTest extends BaseTestCase {
 
         String remains = getSingleHeaderFromJson(responseGetDataRemains, "data", ".InfoStores[0].StoreQuantRem");
         Assertions.assertResponseTextEqualsToAnother(responseGetAuth, rem, remains);
+    }
+
+    @Test
+    @Owner(value = "Дегтярёв Никита Витальевич")
+    @Severity(value = SeverityLevel.CRITICAL)
+    @Description("Это тест входит в аккаунт market, добавляет товар, проверяет в корзине, удаляет и затем опять удаляет")
+    @DisplayName("Проверка работы корзины с добавлением товара")
+    public void testCheckBasketAddingAndDeleting() {
+        String valAdding = "1";
+        String gdsNum = "4541870";
+
+        //LOGIN
+        Response responseGetAuth = apiCoreRequests.
+                makePostRequestNoBody("https://idev.etm.ru/api/ipro/user/login?log=9692161158&pwd=20101999d");
+
+        String sessionId = getSingleHeaderFromJson(responseGetAuth, "data", ".session");
+        Assertions.assertResponseCodeEquals(responseGetAuth, 200);
+
+
+        //ADDING
+        Response responseAddProduct = apiCoreRequests
+                .makeGetRequestWithNoBody("https://idev.etm.ru/api/ipro/basket/add?val=" + valAdding + "&gds=" + gdsNum + "&session-id=" + sessionId);
+        Assertions.assertResponseCodeEquals(responseAddProduct, 200);
+        Assertions.assertJsonByName(responseAddProduct, "data", ".basket_last_line_num", valAdding);
+
+        //CHECK BASKET AFTER ADDING
+        Response responseCheckBasket = apiCoreRequests
+                .makeGetRequestWithNoBody("https://idev.etm.ru/api/ipro/basket?city=78&skl=4280&session-id=" + sessionId);
+        Assertions.assertNotJsonByName(responseCheckBasket, "data", ".sum", "0.0");
+
+        //CLEAR BASKET
+        Response responseClearAllBasket = apiCoreRequests
+                .makePostRequestNoBody("https://idev.etm.ru/api/ipro/basket/clear?session-id=" + sessionId);
+        Assertions.assertResponseCodeEquals(responseClearAllBasket, 200);
+        Assertions.assertJsonByName(responseCheckBasket, "status", ".code", "200");
+
+        //CHECK BASKET CLEARING
+        Response responseCheckBasketAfterCleaning = apiCoreRequests
+                .makeGetRequestWithNoBody("https://idev.etm.ru/api/ipro/basket?city=78&skl=4280&session-id=" + sessionId);
+        Assertions.assertJsonByName(responseCheckBasketAfterCleaning, "data", ".sum", "0.0");
+
+    }
+
+    @Test
+    @Owner(value = "Дегтярёв Никита Витальевич")
+    @Severity(value = SeverityLevel.CRITICAL)
+    @Description("Это тест входит в аккаунт market, добавляет товар, проверяет в корзине, удаляет и затем опять удаляет")
+    @DisplayName("Проверка работы корзины с добавлением товара")
+    public void testAddToCompare() {
+        String from_rus = "1";
+        String spec = "1";
+
+
+
+        //LOGIN
+        Response responseGetAuth = apiCoreRequests.
+                makePostRequestNoBody("https://idev.etm.ru/api/ipro/user/login?log=9692161158&pwd=20101999d");
+
+        String sessionId = getSingleHeaderFromJson(responseGetAuth, "data", ".session");
+        Assertions.assertResponseCodeEquals(responseGetAuth, 200);
+
+        //TAKE DATA FROM CATALOG
+        Response responseGetData = apiCoreRequests.
+                makeGetRequestWithNoBody("https://idev.etm.ru/_next/data/" + urlForCatalog +
+                        "/catalog/101010_kabeli_s_mednoy_tokoprovodjaschey_zhiloy.json?from_rus=" +
+                        from_rus + "&spec=" + spec + "&rows=20&page=1&nameCat=101010_kabeli_s_mednoy_tokoprovodjaschey_zhiloy");
+
+
     }
 
 
